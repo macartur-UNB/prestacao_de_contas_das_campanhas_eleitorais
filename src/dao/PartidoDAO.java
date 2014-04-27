f893f1299fea05a6b3ba1dbd352838dc936c5d43
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-04-27 16:04:09 -0300

    Modificações em PartidoDAO

--- src/dao/PartidoDAO.java
+++ src/dao/PartidoDAO.java
@@ -1,5 +1,14 @@
 package dao;
 
 public class PartidoDAO {
+	
+	private static final String SIGLA_PARTIDO = "sigla";
+	private static final String NUMERO_PARTIDO = "numeroPartido";
+	
+	public PartidoDAO() {
+		
+	}
+	
+	
 
 }
