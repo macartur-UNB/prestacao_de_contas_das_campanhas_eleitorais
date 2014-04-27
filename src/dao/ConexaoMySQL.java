d99fba546ba56114898af68e6eebf1fd93898cfd
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-04-27 16:47:00 -0300

    Modificações de informações para conexão com o banco

--- src/dao/ConexaoMySQL.java
+++ src/dao/ConexaoMySQL.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 public class ConexaoMySQL {
 	
 	private static final String MY_SQL = "jdbc:mysql://";	
@@ -13,22 +12,6 @@ public class ConexaoMySQL {
 	private static final String NOME_BANCO = "gpp";
 	private static final String USUARIO = "root";
 	private static final String SENHA = "root";
-	
-	/** Estou usando como padrao: 
-	 *     NOME_BANCO = "gpp";
-	 *     USUARIO = "root";
-	 *     SENHA = "root";
-	 *  Para a conexao mySQL funcionar, usei no prompt do mysql:
-	 *     $ SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root'); 
-	 *    
-	 *  No caso de mudar o password e nao conseguir acessar o mysql,
-	 *  buscar usuario e senha em 
-	 *     $ vi /etc/mysql/debian.cnf 
-	 *  e acessar com os parametros
-	 *     $ mysql --user=debian-sys-maint --password=GPCzJs63iCz3w3Y8
-	 *     (de acordo com o que constar no arquivo.
-	 *  (RAFAEL, 1/5/14) 
-	 */
 	
 	private static ConexaoMySQL instancia;
 	
