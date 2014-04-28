d963f0b2f982084971257c34158a5aea09304de6
Rafael Valenca <rafa7lima@gmail.com>
2014-04-28 19:12:40 -0300

    Teste de unidade getters e setters de Receita

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,5 +1,17 @@
 package model;
 
 public abstract class Pessoa {
+	
+	private String nome;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
 }
