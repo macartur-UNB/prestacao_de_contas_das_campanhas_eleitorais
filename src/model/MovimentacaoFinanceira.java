32c41bd0f85047b4eb5a847c336ba2fde1c1b77d
Modesto <augusto.modesto@live.com>
2014-04-29 19:01:03 -0300

    Adicionados gets e sets da classe

--- src/model/MovimentacaoFinanceira.java
+++ src/model/MovimentacaoFinanceira.java
@@ -2,11 +2,9 @@ package model;
 
 import java.util.Calendar;
 
-import javax.swing.text.StyledEditorKit.BoldAction;
-
 public class MovimentacaoFinanceira {
 	
-	private Pessoa emNomeDe;
+	private Pessoa emNomeDe; //Classe ainda não criada
 	private String horaRegistro;
 	private boolean entregaEmConjunto;
 	private String numeroDocumento;
@@ -17,4 +15,87 @@ public class MovimentacaoFinanceira {
 	private String especie;
 	private String descricao;
 	
+	public MovimentacaoFinanceira(){
+		
+	}
+
+	public Pessoa getEmNomeDe() {
+		return emNomeDe;
+	}
+
+	public void setEmNomeDe(Pessoa emNomeDe) {
+		this.emNomeDe = emNomeDe;
+	}
+
+	public String getHoraRegistro() {
+		return horaRegistro;
+	}
+
+	public void setHoraRegistro(String horaRegistro) {
+		this.horaRegistro = horaRegistro;
+	}
+
+	public boolean isEntregaEmConjunto() {
+		return entregaEmConjunto;
+	}
+
+	public void setEntregaEmConjunto(boolean entregaEmConjunto) {
+		this.entregaEmConjunto = entregaEmConjunto;
+	}
+
+	public String getNumeroDocumento() {
+		return numeroDocumento;
+	}
+
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public String getFonte() {
+		return fonte;
+	}
+
+	public void setFonte(String fonte) {
+		this.fonte = fonte;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}	
 }
