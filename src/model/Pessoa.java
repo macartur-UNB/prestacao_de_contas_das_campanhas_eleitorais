2e780fc64c76eea7f5350dea5d880752ac958bf9
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2014-04-29 14:35:35 -0300

    Criado a classe Pessoa em model

---
+++ src/model/Pessoa.java
@@ -0,0 +1,16 @@
+package model;
+
+public abstract class Pessoa {
+
+	protected String nome;
+	protected Boolean pessoaJuridica;
+	
+	public Pessoa() {
+	}
+
+	// Coloca Override?
+	// Gera Getters and Setters?
+	// Realiza testes em /testes?
+	// Cria a DAO em /dao?
+	// Cria a controller em /controller?
+}
