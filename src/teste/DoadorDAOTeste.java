27ba3c7c2d564d7ddf69d195c4e425ee639f842d
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2014-04-29 14:06:42 -0300

    Criado DoadorDAOTeste em teste

---
+++ src/teste/DoadorDAOTeste.java
@@ -0,0 +1,35 @@
+package teste;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dao.ConexaoMySQL;
+
+public class DoadorDAOTeste {
+	
+	private ConexaoMySQL conexaoMySQL;
+
+	@Before
+	public void setUp() {
+		this.conexaoMySQL = ConexaoMySQL.getInstancia();
+	}
+
+	@Test
+	public void NaoDeveLancarExcecaoAoIniciarAConexao() throws SQLException {
+		this.conexaoMySQL.iniciarConexao();
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void DeveLancarExcecaoAoEncerrarUmaConexaoQueNaoFoiIniciada() throws SQLException {
+		this.conexaoMySQL.encerrarConexao();
+	}
+	
+	@Test
+	public void NaoDeveLancarExcecaoAoEncerrarUmaConexaoQueFoiIniciada() throws SQLException {
+		this.conexaoMySQL.iniciarConexao();
+		this.conexaoMySQL.encerrarConexao();
+	}
+
+}
