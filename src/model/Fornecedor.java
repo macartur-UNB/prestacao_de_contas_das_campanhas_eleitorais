5936ae0537263ea09556a828fca6476a5b59aa3d
Yuri <yuri@yuri-VirtualBox.(none)>
2014-04-29 13:59:25 -0300

    criação da classe Fornecedor

---
+++ src/model/Fornecedor.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Fornecedor extends Entidade{
+		
+	private Integer cadastroNacional;
+	
+	public Integer getCadastroNacional() {
+		return cadastroNacional;
+	}
+
+	public void setCadastroNacional(Integer arrecadacao) {
+		this.cadastroNacional = cadastroNacional;
+	}
+	
+}
