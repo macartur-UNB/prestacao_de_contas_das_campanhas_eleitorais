261fbae8755c66301b21687eaadce2fa95a38521
Modesto <augusto.modesto@live.com>
2014-04-29 18:58:14 -0300

    Adicionados atributos a classe movimentação

--- src/model/MovimentacaoFinanceira.java
+++ src/model/MovimentacaoFinanceira.java
@@ -1,5 +1,20 @@
 package model;
 
-public class MovimentacaoFinanceira {
+import java.util.Calendar;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+public class MovimentacaoFinanceira {
+	
+	private Pessoa emNomeDe;
+	private String horaRegistro;
+	private boolean entregaEmConjunto;
+	private String numeroDocumento;
+	private Calendar data;
+	private float valor;
+	private String fonte;
+	private String tipo;
+	private String especie;
+	private String descricao;
+	
 }
