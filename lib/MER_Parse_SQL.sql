246774490baf0008dff124b9cb7c0ca234ef5080
Rafael Valenca <rafa7lima@gmail.com>
2014-04-29 15:08:04 -0300

    ReceitaDAO

--- lib/MER_Parse_SQL.sql
+++ lib/MER_Parse_SQL.sql
@@ -13,4 +13,18 @@ CREATE TABLE t_candidato (
   PRIMARY KEY(id)
 );
 
+CREATE  TABLE t_receita (
+  id INT NOT NULL ,
+  emNomeDe VARCHAR(45) NULL ,
+  horaRegistro VARCHAR(45) NULL ,
+  entregaEmConjunto VARCHAR(45) NULL ,
+  numero VARCHAR(45) NULL ,
+  data VARCHAR(45) NULL ,
+  valor VARCHAR(45) NULL ,
+  fonte VARCHAR(45) NULL ,
+  tipo VARCHAR(45) NULL ,
+  especie VARCHAR(45) NULL ,
+  descricao VARCHAR(45) NULL ,
+  PRIMARY KEY (id) 
+);
 
