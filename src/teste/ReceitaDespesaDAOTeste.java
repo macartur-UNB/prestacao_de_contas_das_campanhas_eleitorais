5a5dac72791c53d35b6d4065f1daa48ef058c366
Rafael Valenca <rafa7lima@gmail.com>
2014-05-07 14:46:42 -0300

    get ListaDespesa e Receita recebem como argumento uma Pessoa

---
+++ src/teste/ReceitaDespesaDAOTeste.java
@@ -0,0 +1,29 @@
+/** CRIADO POR:          Rafael Valenca
+ *  ULTIMA MODIFICACAO:  06/05/2014 (Luciano)
+ * 
+ *  COMENTARIOS: Nao verifiquei o cadastro de receitas repetidas porque esse
+ *  nao eh um problema critico como no caso de Candidato e Partido.
+**/
+
+package teste;
+
+import static org.mockito.Mockito.mock;
+import modelo.dao.DespesaDAO;
+import modelo.dao.ReceitaDAO;
+
+import org.junit.Before;
+
+
+public class ReceitaDespesaDAOTeste {
+
+	private ReceitaDAO mockReceitaDAO;
+	private DespesaDAO mockDespesaDAO;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.mockReceitaDAO = mock(ReceitaDAO.class);
+		this.mockDespesaDAO = mock(DespesaDAO.class);
+	}
+
+
+}
