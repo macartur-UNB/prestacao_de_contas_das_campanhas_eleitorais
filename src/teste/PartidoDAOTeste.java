c183f804692de4584170f38d825708fe3a3c6fb0
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-08 14:54:24 -0300

    Início da implementação de testes para DAO do Partido

--- src/teste/PartidoDAOTeste.java
+++ src/teste/PartidoDAOTeste.java
@@ -1,17 +1,39 @@
 package teste;
 
 import static org.mockito.Mockito.mock;
+
+import java.sql.SQLException;
+
+import modelo.dao.ConexaoBancoDados;
 import modelo.dao.PartidoDAO;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 public class PartidoDAOTeste {
 	
 	private PartidoDAO mockPartidoDAO;
+	private Connection conexao;
+	private PreparedStatement instrucaoSQL;
 	
 	@Before
 	public void setUp() throws Exception {
 		this.mockPartidoDAO = mock(PartidoDAO.class);
+	}
+	
+	@Test (expected  = NullPointerException.class)
+	public void deveLancarExcecaoAoFecharUmaConexaoNaoIniciada() throws SQLException {
+		this.instrucaoSQL.close();
+		this.conexao.close();
+	}
+	
+	@Test
+	public void naoDeveLancarExcecaoAoFecharUmaConexaoIniciada() throws SQLException {
+		this.conexao = (Connection) new ConexaoBancoDados().getConexao();
+		this.conexao.close();
 	}
 
 }
