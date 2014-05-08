f8c075428cbf5af7bea3346631adc45096210a48
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-08 11:05:12 -0300

    Testes para validação da sigla do Partido

--- src/teste/PartidoValidacaoTeste.java
+++ src/teste/PartidoValidacaoTeste.java
@@ -1,15 +1,37 @@
 package teste;
 
 import static org.junit.Assert.*;
+import modelo.beans.Partido;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class PartidoValidacaoTeste {
+import controle.excecao.PartidoExcecao;
+import controle.validacao.PartidoValidacao;
 
+public class PartidoValidacaoTeste {
+	
+	private PartidoValidacao partidoValidacao;
+	private Partido partido;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.partidoValidacao = new PartidoValidacao();
+		this.partido = new Partido();
+	}
+
+	@Test (expected = PartidoExcecao.class)
+	public void lancaExcecaoSeSiglaEhNula() throws PartidoExcecao {
+		partido.setSigla(null);
+		
+		this.partidoValidacao.siglaNaoNula(partido);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void naoLancaExcecaoSeSiglaNaoEhNula() throws PartidoExcecao {
+		partido.setSigla("PT");
+		
+		this.partidoValidacao.siglaNaoNula(partido);
 	}
 
 }
