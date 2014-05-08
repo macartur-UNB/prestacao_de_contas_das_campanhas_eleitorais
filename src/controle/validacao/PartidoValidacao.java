dac3258ecf2a5fbfa69b488fe243e907abdd849b
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-08 10:52:58 -0300

    Adicionando Classes para Validação e Exceção para Partido

---
+++ src/controle/validacao/PartidoValidacao.java
@@ -0,0 +1,27 @@
+package controle.validacao;
+
+import controle.excecao.PartidoExcecao;
+import modelo.beans.Partido;
+
+public class PartidoValidacao {
+
+	private static final String SIGLA_VAZIA = "Campo Sigla do Partido vazia!";
+	private static final String NUMERO_PARTIDO_VAZIO = "Campo Número do Partido vazio!";
+
+	public PartidoValidacao() {
+
+	}
+
+	public void siglaNaoNula(Partido partido) throws PartidoExcecao {
+		if ((partido.getSigla() == null) || (partido.getSigla().length() == 0)) {
+			throw new PartidoExcecao(SIGLA_VAZIA);
+		}
+	}
+	
+	public void numeroNaoNulo(Partido partido) throws PartidoExcecao {
+		if((partido.getNumeroPartido() == null)) {
+			throw new PartidoExcecao(NUMERO_PARTIDO_VAZIO);
+		}
+	}
+
+}
