7062d69752c14f75e4acd7be30a636389f8d6c6d
Rafael Valenca <rafa7lima@gmail.com>
2014-05-08 15:11:56 -0300

    Criacao da servlet

---
+++ src/controle/servlet/SelecionarReceitasEDespesas.java
@@ -0,0 +1,24 @@
+package controle.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SelecionarReceitasEDespesas")
+public class SelecionarReceitasEDespesas extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/selecionar_receitas_despesas.jsp");
+		requestDispatcher.forward(request, response);
+	}
+}
