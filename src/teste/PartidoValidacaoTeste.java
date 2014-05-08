775ecd1f8a18925b6f42c789481422892b9dbd3b
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-08 10:57:30 -0300

    Adicionando Classe de teste para Validação de Informações do Partido

---
+++ src/teste/PartidoValidacaoTeste.java
@@ -0,0 +1,15 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartidoValidacaoTeste {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
