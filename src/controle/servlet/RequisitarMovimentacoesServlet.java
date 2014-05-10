602c3087ff4bb6a34d9681ccebe15577e221d430
Rafael Valenca <rafa7lima@gmail.com>
2014-05-10 19:48:48 -0300

    Desenhei a tabela pra visualizacao dos dados de receita e despesa

--- src/controle/servlet/RequisitarMovimentacoesServlet.java
+++ src/controle/servlet/RequisitarMovimentacoesServlet.java
@@ -73,11 +73,84 @@ public class RequisitarMovimentacoesServlet extends HttpServlet {
 						", " + candidato.getAno() + "</p>");
 			out.println("<br />");
 			
+			//Tabela Receitas 
 			out.println("<h2> Receitas: </h2>");
-			out.println(listaReceitas);
+
+			out.println("<table border=\"2\">");
 			
+			out.println("<tr>");
+				out.println("<th>Hora Registro</th>");
+				//out.println("<th>Entraga em Conjunto</th>");
+				out.println("<th>Número do Documento</th>");
+				out.println("<th>Data</th>");
+				out.println("<th>Valor</th>");
+				out.println("<th>Fonte</th>");
+				out.println("<th>Tipo</th>");
+				out.println("<th>Espécie</th>");
+				out.println("<th>Descrição</th>");
+				out.println("<th>Recibo Eleitoral</th>");
+				out.println("<th>Nome do Doador</th>");
+				out.println("<th>Cadastro do Doador</th>");	
+			out.println("</tr>");
+			for(Receita receita:listaReceitas)
+			{
+			out.println("<tr>");
+				out.println("<td>"+receita.getHoraRegistro()+"</td>");
+				//out.println("<td>"+receita.isEntregaEmConjunto()+"</td>");
+				out.println("<td>"+receita.getNumeroDocumento()+"</td>");
+				out.println("<td>"+receita.getData()+"</td>");
+				out.println("<td>"+receita.getValor()+"</td>");
+				out.println("<td>"+receita.getFonte()+"</td>");
+				out.println("<td>"+receita.getTipo()+"</td>");
+				out.println("<td>"+receita.getEspecie()+"</td>");
+				out.println("<td>"+receita.getDescricao()+"</td>");
+				out.println("<td>"+receita.getReciboEleitoral()+"</td>");
+				out.println("<td>"+receita.getDoador().getNome()+"</td>");
+				out.println("<td>"+receita.getDoador().getCadastroNacional()
+						+"</td>");
+
+			out.println("</tr>");	
+			}
+			out.println("</table>");
+			
+			//Tabela Despesas
 			out.println("<h2> Despesas: </h2>");
-			out.println(listaDespesas);
+			
+			out.println("<table border=\"2\">");
+			out.println("<tr>");
+				out.println("<th>Hora Registro</th>");
+				//out.println("<th>Entraga em Conjunto</th>");
+				out.println("<th>Número do Documento</th>");
+				out.println("<th>Data</th>");
+				out.println("<th>Valor</th>");
+				out.println("<th>Fonte</th>");
+				out.println("<th>Tipo</th>");
+				out.println("<th>Espécie</th>");
+				out.println("<th>Descrição</th>");
+				out.println("<th>Recibo Eleitoral</th>");
+				out.println("<th>Nome do Doador</th>");
+				out.println("<th>Cadastro do Doador</th>");	
+			out.println("</tr>");
+			for(Despesa despesa:listaDespesas)
+			{
+			out.println("<tr>");
+				out.println("<td>"+despesa.getHoraRegistro()+"</td>");
+				//out.println("<td>"+despesa.isEntregaEmConjunto()+"</td>");
+				out.println("<td>"+despesa.getNumeroDocumento()+"</td>");
+				out.println("<td>"+despesa.getData()+"</td>");
+				out.println("<td>"+despesa.getValor()+"</td>");
+				out.println("<td>"+despesa.getFonte()+"</td>");
+				out.println("<td>"+despesa.getTipo()+"</td>");
+				out.println("<td>"+despesa.getEspecie()+"</td>");
+				out.println("<td>"+despesa.getDescricao()+"</td>");
+				out.println("<td>"+despesa.getTipoDocumento()+"</td>");
+				out.println("<td>"+despesa.getFornecedor().getNome()+"</td>");
+				out.println("<td>"+despesa.getFornecedor().getCadastroNacional()
+						+"</td>");
+
+			out.println("</tr>");	
+			}
+			out.println("</table>");
 			
 			out.println("</body>");
 			out.println("</html>");
