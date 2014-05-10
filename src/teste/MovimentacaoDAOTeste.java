e33e31924cf4fc725970bfce1296f78637be1d5f
Rafael Valenca <rafa7lima@gmail.com>
2014-05-10 13:58:43 -0300

    Criada pagina de requisicao de Relatorio de Movimetacoes Financeiras; Uni ReceitaDAO e DespesaDAO em uma unica classe MovimentacaoDAO

--- src/teste/MovimentacaoDAOTeste.java
+++ src/teste/MovimentacaoDAOTeste.java
@@ -13,22 +13,20 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 
 import modelo.beans.Candidato;
+import modelo.beans.Despesa;
 import modelo.beans.Receita;
-import modelo.dao.DespesaDAO;
-import modelo.dao.ReceitaDAO;
+import modelo.dao.MovimentacaoDAO;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class ReceitaDespesaDAOTeste {
+public class MovimentacaoDAOTeste {
 
-	private ReceitaDAO mockReceitaDAO;
-	private DespesaDAO mockDespesaDAO;
+	private MovimentacaoDAO mockMovimentacaoDAO;
 
 	@Before
 	public void setUp() throws Exception {
-		this.mockReceitaDAO = mock(ReceitaDAO.class);
-		this.mockDespesaDAO = mock(DespesaDAO.class);
+		this.mockMovimentacaoDAO = mock(MovimentacaoDAO.class);
 		
 	}
 
@@ -36,7 +34,7 @@ public class ReceitaDespesaDAOTeste {
 	public void obterListaReceitas() throws SQLException {
 		Candidato candidato = new Candidato();
 		candidato.setNome("Rafael Valenca");
-		LinkedList<Receita> listaReceitas = mockReceitaDAO.getListaReceitas(candidato);
+		LinkedList<Receita> listaReceitas = mockMovimentacaoDAO.getListaReceitas(candidato);
 		System.out.println(listaReceitas.size());
 		//for(Receita receita:listaReceitas)
 		//{
@@ -45,18 +43,14 @@ public class ReceitaDespesaDAOTeste {
 
 	}
 
-	/**
 	@Test
 	public void obterListaDespesas() throws SQLException {
-		Despesa despesa = new Despesa();
-		Fornecedor fornecedor = new Fornecedor();
-		fornecedor.setNome("Jose");
-		despesa.setFornecedor(fornecedor);
-		despesa.setDescricao("Impressao de panfletos.");
+		Candidato candidato = new Candidato();
+		candidato.setNome("Rafael Valenca");
+		LinkedList<Despesa> listaDespesas = mockMovimentacaoDAO.getListaDespesas(candidato);
+		System.out.println(listaDespesas.size());
 
-		this.mockDespesaDAO.cadastrarDespesa(despesa);
-		
+
 	}
-	*/
 
 }
