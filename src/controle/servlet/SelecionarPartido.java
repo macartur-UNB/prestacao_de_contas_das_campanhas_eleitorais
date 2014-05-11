8208382d3b93bdb1c9f11a4b9acf63add9659785
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-11 01:08:52 -0300

    Modificações da Servlet

--- src/controle/servlet/SelecionarPartido.java
+++ src/controle/servlet/SelecionarPartido.java
@@ -1,6 +1,7 @@
 package controle.servlet;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,17 +10,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modelo.beans.Partido;
+import modelo.dao.PartidoDAO;
+
 @WebServlet("/SelecionarPartido")
 public class SelecionarPartido extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	PartidoDAO partidoDAO = new PartidoDAO();
+	RequestDispatcher rd;
+	HttpServletRequest request;
+	HttpServletResponse response;
+	
+	public SelecionarPartido() {
+		
+	}
        
-	@Override
+	/*@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/visualizar_partido.jsp");
 		requestDispatcher.forward(request, response);
+	}*/
+	
+	public void exibirInformacaoPartido(LinkedList<Partido> listaPartidos) {
+		
+		this.partidoDAO = partidoDAO;
+		this.request = request;
+		this.response = response;
+		
+		
 	}
 	
 	
