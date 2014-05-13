fb1ba08f1b1a33b657e32f1b3924aba11b201600
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-13 20:36:02 -0300

    Aprimoramento e teste para a PartidoControle

---
+++ src/teste/PartidoControleTeste.java
@@ -0,0 +1,72 @@
+package teste;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.beans.Partido;
+import modelo.dao.PartidoDAO;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import controle.PartidoControle;
+
+public class PartidoControleTeste extends TemplateTeste {
+	
+	private PartidoDAO partidoDAO;
+	private PartidoControle partidoControle;
+	
+	@Override
+	public void beforeTest() throws Exception {
+		this.partidoControle = new PartidoControle();
+		this.partidoDAO = new PartidoDAO();
+	}
+
+	@Override
+	public void afterTest() throws Exception {
+		
+	}
+
+	@Test
+	public void deveRecuperarUmPartidoPelaSigla() throws SQLException {
+		
+		ArrayList<Partido> listaPartidos = new ArrayList<>();
+		
+		Partido partido = new Partido();
+		partido.setNumeroPartido("13");
+		partido.setSigla("PT");
+		listaPartidos.add(partido);
+		
+		this.partidoDAO.cadastrarPartidos(listaPartidos);
+		this.partidoDAO.getPartido("PT");
+		this.partidoControle.getPartido("PT");
+		
+		Assert.assertEquals(partido, this.partidoControle.getPartido("PT"));
+	}
+	
+	@Test
+	public void deveRecuperarUmaListaDePartidos() throws SQLException {
+		
+		ArrayList<Partido> listaPartidos = new ArrayList<>();
+		
+		Partido partido1 = new Partido();
+		partido1.setSigla("PSDB");
+		partido1.setNumeroPartido("45");
+		listaPartidos.add(partido1);
+		
+		Partido partido2 = new Partido();
+		partido2.setSigla("PT");
+		partido2.setNumeroPartido("13");
+		listaPartidos.add(partido2);
+		
+		this.partidoDAO.cadastrarPartidos(listaPartidos);
+		this.partidoDAO.getListaPartidos();
+		this.partidoControle.getListaPartidos();
+		
+		Assert.assertEquals(listaPartidos, this.partidoControle.getListaPartidos());
+		
+	}
+
+	
+
+}
