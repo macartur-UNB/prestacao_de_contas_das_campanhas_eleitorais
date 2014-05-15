7b2509cd4b11ca78331d0af82d36af20646da448
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-05-15 18:35:42 -0300

    Revisão e Aprimoramentos

--- src/controle/servlet/SelecionarCandidato.java
+++ src/controle/servlet/SelecionarCandidato.java
@@ -13,12 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import controle.CandidatoControle;
 import modelo.beans.Candidato;
 
-
 @WebServlet("/SelecionarCandidato")
 public class SelecionarCandidato extends HttpServlet {
 
 	private static final long serialVersionUID = -4024368294265814535L;
-	
+
 	@Override
 	protected void service(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
@@ -26,28 +25,26 @@ public class SelecionarCandidato extends HttpServlet {
 		String nome = request.getParameter("nome");
 		Candidato candidato = new Candidato();
 		candidato.setNome(nome);
-		
+
 		RequestDispatcher requestDispatcher;
-		
+
 		CandidatoControle control = new CandidatoControle();
-		List<Candidato> listaCandidato = (List<Candidato>) control.getCandidato(nome);
-		
-		// verifica se candidato existe
-		if(listaCandidato.isEmpty()){
+		List<Candidato> listaCandidato = (List<Candidato>) control
+				.getCandidato(nome);
+
+		if (listaCandidato.isEmpty()) {
 			requestDispatcher = request
-				    .getRequestDispatcher("/erro_candidato_inexistente.jsp");
-			requestDispatcher.forward(request,response);
-		} else{
-	
+					.getRequestDispatcher("/erro_candidato_inexistente.jsp");
+			requestDispatcher.forward(request, response);
+		} else {
+
 			String cpf = listaCandidato.get(0).getCpf();
 			request.setAttribute("cpf", cpf);
-			request.setAttribute("listaCandidato",listaCandidato);
-		
+			request.setAttribute("listaCandidato", listaCandidato);
+
 			requestDispatcher = request
-				.getRequestDispatcher("/visualizar_candidato.jsp");
+					.getRequestDispatcher("/visualizar_candidato.jsp");
 			requestDispatcher.forward(request, response);
-		
-		
 		}
 	}
 
