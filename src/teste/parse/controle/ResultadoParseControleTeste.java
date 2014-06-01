e5c93423fe0bd0437d1e46c98b675f6c861b3ae5
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-06-01 18:39:53 -0300

    Aprimoramento dos testes unitários para resultado

---
+++ src/teste/parse/controle/ResultadoParseControleTeste.java
@@ -0,0 +1,70 @@
+package teste.parse.controle;
+
+import modelo.beans.Resultado;
+import modelo.dao.ResultadoDAO;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import parse.controle.ResultadoParseControle;
+import parse.indices.ResultadoIndicesParse;
+import teste.TemplateTeste;
+
+public class ResultadoParseControleTeste extends TemplateTeste {
+
+	public static final int ID = 0;
+	public static final int CODIGO = 1;
+	public static final int DESCRICAO = 2;
+	
+	private String campo[];
+	private ResultadoDAO resultadoDAO;
+	private ResultadoIndicesParse resultadoIndicesParse;
+	private ResultadoParseControle resultadoParseControle;
+
+	public void beforeTest() throws Exception {
+		this.campo = new String[3];
+		this.resultadoDAO = new ResultadoDAO();
+		this.resultadoIndicesParse = new ResultadoIndicesParse();
+		this.resultadoParseControle = new ResultadoParseControle(this.resultadoIndicesParse);
+		
+		iniciarCampos();
+		iniciarIndices();
+	}
+	
+	public void afterTest() {
+		
+	}
+
+	
+	@Test
+	public void cadastrarResultados() throws Exception {
+		
+		this.resultadoParseControle.addInstancia(campo);
+		this.resultadoParseControle.cadastrarInstancias();
+		this.resultadoParseControle.resetar();
+		
+		Resultado resultadoCadastrado = this.resultadoDAO.getLista().get(0);
+				
+		Assert.assertEquals(this.campo[ID], resultadoCadastrado.getId().toString());
+		Assert.assertEquals(this.campo[CODIGO], resultadoCadastrado.getCodigo().toString());
+		Assert.assertEquals(this.campo[DESCRICAO], resultadoCadastrado.getDescricao());
+		
+	}
+	
+	private void iniciarIndices() {
+		
+		this.resultadoIndicesParse.setIndiceId(ID);
+		this.resultadoIndicesParse.setIndiceCodigo(CODIGO);
+		this.resultadoIndicesParse.setIndiceDescricao(DESCRICAO);
+		
+	}
+	
+	private void iniciarCampos() {
+		
+		this.campo[ID] = "1";
+		this.campo[CODIGO] = "125";
+		this.campo[DESCRICAO] = "RESULTADO INEXISTENTE";
+		
+	}
+
+}
