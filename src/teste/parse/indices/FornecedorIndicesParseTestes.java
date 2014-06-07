9f613a02b26012900f6f4dd1828b1d6738fdb72a
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-06-07 13:10:23 -0300

    Aprimoramento de testes

--- src/teste/parse/indices/FornecedorIndicesParseTestes.java
+++ src/teste/parse/indices/FornecedorIndicesParseTestes.java
@@ -1,67 +1,68 @@
 package teste.parse.indices;
 
-import static org.junit.Assert.*;
 import modelo.beans.Fornecedor;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import parse.indices.FornecedorIndicesParse;
-import teste.TemplateTeste;
 
-public class FornecedorIndicesParseTestes extends TemplateTeste {
-	
-	public static final int INDICE_NOME = 0;
-	public static final int INDICE_CADASTRO_NACIONAL = 0;
+public class FornecedorIndicesParseTestes {
 	
 	private String campo[];
 	private FornecedorIndicesParse fornecedorIndicesParse;
 
-	@Override
-	public void beforeTest() throws Exception {
-		this.campo = new String[2];
+	@Before
+	public void setUp() throws Exception {
 		this.fornecedorIndicesParse = new FornecedorIndicesParse();
+		this.campo = new String[4];
 		
 		iniciarCampos();
 		iniciarIndices();
-	}
-
-	@Override
-	public void afterTest() throws Exception {
-		
 	}
 	
 	@Test
 	public void iniciarUmFornecedorComIndicesValidos() throws Exception {
 		Fornecedor fornecedor = new Fornecedor();
 		this.fornecedorIndicesParse.iniciarInstancia(fornecedor, campo);
-		assertEquals(this.campo[INDICE_NOME], fornecedor.getNome());
-		assertEquals(this.campo[INDICE_CADASTRO_NACIONAL], fornecedor.getCadastroNacional());
+		Assert.assertEquals(this.campo[0], fornecedor.getCpf_cnpj().toString());
+		Assert.assertEquals(this.campo[1], fornecedor.getNome());
+		Assert.assertEquals(this.campo[2], fornecedor.getUf());
+		Assert.assertEquals(this.campo[3], fornecedor.getSituacaoCadastral());
 	}
 	
-	
 	@Test
-	public void iniciarUmCandidatoComIndicesInvalidos() throws Exception {
+	public void iniciarUmFornecedorComIndicesInvalidos() {
 		this.fornecedorIndicesParse = new FornecedorIndicesParse();
 		Fornecedor fornecedor = new Fornecedor();
 		this.fornecedorIndicesParse.iniciarInstancia(fornecedor, campo);
-		assertNotEquals(this.campo[INDICE_NOME], fornecedor.getNome());
-		assertNotEquals(this.campo[INDICE_CADASTRO_NACIONAL], fornecedor.getCadastroNacional());
-	}
-	
-	@Test
-	public void indicesDevemSerIguais() throws Exception {
-		assertEquals(INDICE_NOME, this.fornecedorIndicesParse.getIndiceNome());
-		assertEquals(INDICE_CADASTRO_NACIONAL, this.fornecedorIndicesParse.getIndiceCadastroNacional());
+		Assert.assertNotEquals(this.campo[0], fornecedor.getCpf_cnpj().toString());
+		Assert.assertNotEquals(this.campo[1], fornecedor.getNome());
+		Assert.assertNotEquals(this.campo[2], fornecedor.getUf());
+		Assert.assertNotEquals(this.campo[3], fornecedor.getSituacaoCadastral());
 	}
 	
 	private void iniciarIndices() {
-		this.fornecedorIndicesParse.setIndiceNome(INDICE_NOME);
-		this.fornecedorIndicesParse.setIndiceCadastroNacional(INDICE_CADASTRO_NACIONAL);
+		this.fornecedorIndicesParse.setIndiceCpf_Cnpj(0);
+		this.fornecedorIndicesParse.setIndiceNome(1);
+		this.fornecedorIndicesParse.setIndiceUf(2);
+		this.fornecedorIndicesParse.setIndiceSituacaoCadastral(3);
 	}
 	
 	private void iniciarCampos() {
-		this.campo[INDICE_NOME] = "NOME";
-		this.campo[INDICE_CADASTRO_NACIONAL] = "123123";
+		this.campo[0] = "55325424149";
+		this.campo[1] = "VANDERLEI";
+		this.campo[2] = "DF";
+		this.campo[3] = "REGULAR";
+	}
+	
+	@Test
+	public void verificarIndices() {
+		Assert.assertEquals(0, this.fornecedorIndicesParse.getIndiceCpf_Cnpj());
+		Assert.assertEquals(1, this.fornecedorIndicesParse.getIndiceNome());
+		Assert.assertEquals(2, this.fornecedorIndicesParse.getIndiceUf());
+		Assert.assertEquals(3, this.fornecedorIndicesParse.getIndiceSituacaoCadastral());
 	}
 	
 }
