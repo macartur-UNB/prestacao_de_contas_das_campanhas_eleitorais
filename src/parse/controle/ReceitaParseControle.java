309cfa22b71f23ecf337222b6ca4b178080cee89
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-06-07 22:31:57 -0300

    Aperfeiçoamento do Parse de Receita - Controle e Índices

--- src/parse/controle/ReceitaParseControle.java
+++ src/parse/controle/ReceitaParseControle.java
@@ -1,5 +1,24 @@
 package parse.controle;
 
-public class ReceitaParseControle {
+import modelo.beans.Receita;
+import modelo.dao.ReceitaDAO;
+import parse.indices.IndicesParse;
+
+public class ReceitaParseControle extends ParseControle<Receita> {
+
+	public ReceitaParseControle(IndicesParse<Receita> indicesParse) {
+		super(indicesParse, new ReceitaDAO());
+	}
+
+	@Override
+	public Receita novaInstancia() {
+		Receita receita = new Receita();
+		return receita;
+	}
+
+	@Override
+	public boolean iguais(Receita objetoUm, Receita objetoDois) {
+		return objetoUm.equals(objetoDois);
+	}
 
 }
