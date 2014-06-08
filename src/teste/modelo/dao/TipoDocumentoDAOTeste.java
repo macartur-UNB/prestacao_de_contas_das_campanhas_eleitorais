dd552fe7650350210b5233566ee7cc230d8d4d43
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-06-08 15:37:14 -0300

    Aprimoramentos de testes unitários

---
+++ src/teste/modelo/dao/TipoDocumentoDAOTeste.java
@@ -0,0 +1,23 @@
+package teste.modelo.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import teste.TemplateTeste;
+
+public class TipoDocumentoDAOTeste extends TemplateTeste {
+
+	@Override
+	public void beforeTest() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterTest() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
