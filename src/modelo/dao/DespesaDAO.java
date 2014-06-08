4eb72c2d1b7164645f1bb662f1a4181d59f0f968
Rafael Valenca <rafa7lima@gmail.com>
2014-06-07 21:03:27 -0300

    Autores: Matheus e Rafael
    DespesaDAO

--- src/modelo/dao/DespesaDAO.java
+++ src/modelo/dao/DespesaDAO.java
@@ -6,7 +6,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+import modelo.beans.Campanha;
 import modelo.beans.Despesa;
+import modelo.beans.FormaPagamento;
+import modelo.beans.Fornecedor;
+import modelo.beans.TipoDocumento;
+import modelo.beans.TipoMovimentacao;
 import parse.ParseDAO;
 
 public class DespesaDAO extends BasicoDAO<Despesa> implements ParseDAO<Despesa>{
@@ -32,59 +37,108 @@ public class DespesaDAO extends BasicoDAO<Despesa> implements ParseDAO<Despesa>{
 	private TipoDocumentoDAO tipoDocumentoDAO;
 	private FornecedorDAO fornecedorDAO;
 		
-	private static final String NOME_TABELA = "campanha";
-	private final String ID = "id_Campanha";
-	private final String COD_RESULTADO = "resultado_cod_resultado";
-	private final String COD_CARGO = "cargo_cod_cargo";
-	private final String SIGLA_PARTIDO = "partido_sigla";
-	private final String TITULO_CANDIDATO = "candidato_titulo_eleitoral";
+	private static final String NOME_TABELA = "despesa";
+	private final String ID = "id_despesa";
+	private final String ID_TIPO_MOVIMENTACAO = "tipo_movimentacao_id_tipo_movimentacao";
+	private final String ID_TIPO_DOCUMENTO = "tipo_documento_id_tipo_documento";
+	private final String ID_FORMA_PAGAMENTO = "forma_de_pagamento_id_forma_pagamento";
+	private final String ID_CAMPANHA = "campanha_id_campanha";
+	private final String CPF_CNPJ_FORNECEDOR = "fornecedor_cpf_cnpj";
+	private final String NUMERO_DOCUMENTO = "numero_documento";
 	private final String ANO = "ano";
-	private final String NUM_CANDIDATO = "numero_candidatura";
-	private final String NOME_URNA = "nome_de_urna";
-	private final String UF = "uf";
-	private final String DESPESA_MAX_DECLARADA = "despesa_maxima_declarada";
-	private final String DESPESA_MAX_CALCULADA = "despesa_maxima_calculada";
-	private final String RECEITA_MAX_CALCULADA = "receita_maxima_calculada";
+	private final String VALOR = "valor";
+	private final String DESCRICAO = "descricao";
+	private final String DATA = "data_despesa";	
 	
 	private final String SQL_SELECT = "SELECT * FROM " + NOME_TABELA;
 	private final String SQL_INSERT = "INSERT INTO "
-					   + NOME_TABELA + " (" + ID + ", " + COD_RESULTADO + ", "
-					   + COD_CARGO + ", " + SIGLA_PARTIDO + ", " 
-					   + TITULO_CANDIDATO + ", " + ANO + ", " + NUM_CANDIDATO
-					   + ", " + NOME_URNA + ", " + UF + ", "
-					   + DESPESA_MAX_DECLARADA + ", " + DESPESA_MAX_CALCULADA
-					   + ", " + RECEITA_MAX_CALCULADA 
-					   + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+					   + NOME_TABELA + " (" + ID + ", " + ID_TIPO_MOVIMENTACAO + ", "
+					   + ID_TIPO_DOCUMENTO + ", " + ID_FORMA_PAGAMENTO + ", " + ID_CAMPANHA
+					   + ", " + CPF_CNPJ_FORNECEDOR + ", " + NUMERO_DOCUMENTO + ", "
+					   + VALOR + ", " + DESCRICAO + ", " + DATA
+					   + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 	
 
 	public DespesaDAO(String nomeTabela, Comparator<Despesa> comparador) {
-		super(nomeTabela, comparador);
-		// TODO Auto-generated constructor stub
+		super(NOME_TABELA, Comparacao.ANO_E_NUMERO);
+		this.campanhaDAO = new CampanhaDAO();
+		this.tipoMovimentacaoDAO = new TipoMovimentacaoDAO();
+		this.formaPagamentoDAO = new FormaPagamentoDAO();
+		this.tipoDocumentoDAO = new TipoDocumentoDAO();
+		this.fornecedorDAO = new FornecedorDAO();
 	}
 
 	@Override
 	protected String getSqlInsert() {
-		// TODO Auto-generated method stub
-		return null;
+		return SQL_INSERT;
 	}
 
 	@Override
 	protected String getSqlSelect() {
-		// TODO Auto-generated method stub
-		return null;
+		return SQL_SELECT;
 	}
 
 	@Override
 	protected void adicionarListaNoBatch(ArrayList<Despesa> lista,
 			PreparedStatement instrucaoSQL) throws SQLException {
-		// TODO Auto-generated method stub
+		for (Despesa despesa : lista) {
+			instrucaoSQL.setInt(1, despesa.getId());			
+			instrucaoSQL.setInt(2, despesa.getTipoMovimentacao().getId());
+			instrucaoSQL.setInt(3, despesa.getTipoDocumento().getId());
+			instrucaoSQL.setInt(4, despesa.getFormaPagamento().getId());
+			instrucaoSQL.setInt(5, despesa.getCampanha().getId());
+			instrucaoSQL.setString(6, despesa.getFornecedor().getCpf_cnpj());	
+			instrucaoSQL.setString(7, despesa.getNumeroDocumento());
+			instrucaoSQL.setInt(8, despesa.getAno());
+			instrucaoSQL.setFloat(9, despesa.getValor());	
+			instrucaoSQL.setString(10, despesa.getDescricao());
+			instrucaoSQL.setString(11, despesa.getData());	
+			instrucaoSQL.addBatch();
+		}
 		
 	}
 
 	@Override
 	protected void adicionarResultSetNaLista(ArrayList<Despesa> lista,
 			ResultSet resultadoSQL) throws SQLException {
-		// TODO Auto-generated method stub
+		while (resultadoSQL.next()) {
+			Campanha campanha = new Campanha();
+			TipoMovimentacao tipoMovimentacao = new TipoMovimentacao();
+			TipoDocumento tipoDocumento = new TipoDocumento();
+			FormaPagamento formaPagamento = new FormaPagamento();
+			Fornecedor fornecedor = new Fornecedor();
+			
+			PreparaCampos(campanha, tipoMovimentacao, tipoDocumento, formaPagamento, 
+					fornecedor, resultadoSQL);
+			
+			Despesa despesa = new Despesa();
+			despesa.setId(resultadoSQL.getInt(ID));			
+			despesa.setAno(resultadoSQL.getInt(ANO));
+			despesa.setCampanha(campanha);
+			despesa.setData(resultadoSQL.getString(DATA));
+			despesa.setDescricao(resultadoSQL.getString(DESCRICAO));
+			despesa.setFormaPagamento(formaPagamento);
+			despesa.setFornecedor(fornecedor);
+			despesa.setNumeroDocumento(resultadoSQL.getString(NUMERO_DOCUMENTO));
+			despesa.setTipoDocumento(tipoDocumento);
+			despesa.setTipoMovimentacao(tipoMovimentacao);
+			despesa.setValor(resultadoSQL.getFloat(VALOR));
+			
+			lista.add(despesa);
+
+		}
+	}
+	
+	private void PreparaCampos(Campanha campanha, TipoMovimentacao tipoMovimentacao,
+			TipoDocumento tipoDocumento, FormaPagamento formaPagamento, 
+			Fornecedor fornecedor,ResultSet resultadoSQL) 
+				throws SQLException {
+		
+		campanha.setId(resultadoSQL.getInt(ID_CAMPANHA));		
+		tipoMovimentacao.setId(resultadoSQL.getInt(ID_TIPO_MOVIMENTACAO));		
+		tipoDocumento.setId(resultadoSQL.getInt(ID_TIPO_DOCUMENTO));
+		formaPagamento.setId(resultadoSQL.getInt(ID_FORMA_PAGAMENTO));
+		fornecedor.setCpf_cnpj(CPF_CNPJ_FORNECEDOR);
 		
 	}
 }
