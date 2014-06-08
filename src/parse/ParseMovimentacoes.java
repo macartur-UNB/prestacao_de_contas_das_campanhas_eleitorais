e2f69d7339879fb7d4f9fbefe1d4a4bcb5bfdd22
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-06-08 18:52:17 -0300

    Refatoração e Verificação dos Pacotes

--- src/parse/ParseMovimentacoes.java
+++ src/parse/ParseMovimentacoes.java
@@ -5,11 +5,8 @@ import java.util.ArrayList;
 import parse.cadastro.CadastroParse;
 import parse.cadastro.receita_despesa.CadastroDespesaParse;
 import parse.cadastro.receita_despesa.CadastroDoadorParse;
-import parse.cadastro.receita_despesa.CadastroFormaPagamentoParse;
 import parse.cadastro.receita_despesa.CadastroFornecedorParse;
 import parse.cadastro.receita_despesa.CadastroReceitaParse;
-import parse.cadastro.receita_despesa.CadastroTipoDocumentoParse;
-import parse.cadastro.receita_despesa.CadastroTipoMovimentacaoParse;
 
 public class ParseMovimentacoes extends Parse {
 	
@@ -25,9 +22,6 @@ public class ParseMovimentacoes extends Parse {
 
 		listaCadastrosParse.add(new CadastroDoadorParse(tipoArquivo, ano));
 		listaCadastrosParse.add(new CadastroFornecedorParse(tipoArquivo, ano));
-		listaCadastrosParse.add(new CadastroFormaPagamentoParse(tipoArquivo, ano));
-		listaCadastrosParse.add(new CadastroTipoMovimentacaoParse(tipoArquivo, ano));
-		listaCadastrosParse.add(new CadastroTipoDocumentoParse(tipoArquivo, ano));
 		listaCadastrosParse.add(new CadastroReceitaParse(tipoArquivo, ano));
 		listaCadastrosParse.add(new CadastroDespesaParse(tipoArquivo, ano));
 
