28131788ed12e22123d5a1e4eb7c9488056269bc
Modesto <augusto.modesto@live.com>
2014-06-14 19:30:22 -0300

    Criado novo teste para cadastro de despesas

---
+++ src/teste/parse/cadastro/CadastroDespesaParseTeste.java
@@ -0,0 +1,114 @@
+package teste.parse.cadastro;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.beans.Despesa;
+import modelo.dao.DespesaDAO;
+
+import org.junit.Test;
+
+import parse.ParseException;
+import parse.cadastro.receita_despesa.CadastroDespesaParse;
+import teste.TemplateTeste;
+
+public class CadastroDespesaParseTeste extends TemplateTeste {
+
+	private CadastroDespesaParse cadastro1;
+	private CadastroDespesaParse cadastro2;
+	private CadastroDespesaParse cadastro3;
+	private DespesaDAO despesaDAO;
+	String  tipoArquivo = "despesa";
+	String  ano1         = "2006";
+	String  ano2         = "2002";
+	String  ano3         = "2010";
+	
+	@Override
+	public void beforeTest() throws Exception {
+		this.cadastro1 = 
+				new CadastroDespesaParse(this.tipoArquivo, this.ano1);
+		this.cadastro2 = 
+				new CadastroDespesaParse(this.tipoArquivo, this.ano2);
+		this.cadastro3 = 
+				new CadastroDespesaParse(this.tipoArquivo, this.ano3);
+		this.despesaDAO = new DespesaDAO();
+	}
+
+	@Override
+	public void afterTest() throws Exception {
+		
+	}
+	
+	@Test
+	public void deveRetornarUmaDespesaCadastradoPara2006() throws ParseException, SQLException {
+		String campo[] = new String[50];
+		campo[3] = "123";
+		campo[1] = "Cargo";
+		campo[11] = "TipoMov";
+		campo[16] = "TipoDoc";
+		campo[13] = "FormaPag";
+		campo[15] = "1234";
+		campo[10] = "Data";
+		campo[19] = "12345";
+		campo[18] = "123456";
+		campo[9] = "1234567";
+		cadastro1.executarLinhaDoArquivo(campo);
+		cadastro1.cadastrarInstancias();
+		
+		ArrayList<Despesa> listaDespesa = despesaDAO.getLista();
+		System.out.println(listaDespesa.get(0).getTipoMovimentacao());
+		assertEquals(listaDespesa.get(0).getTipoMovimentacao(), "TipoMov");
+		
+	}
+	
+	@Test
+	public void deveRetornarUmaDespesaCadastradoPara2002() throws ParseException, SQLException {
+		String campo[] = new String[50];
+		campo[4] = "123";
+		campo[2] = "Cargo";
+		campo[10] = "TipoMov";
+		campo[5] = "Data";
+		campo[6] = "1234";
+		campo[8] = "FornecedorNome";
+		campo[9] = "12345";
+		cadastro2.executarLinhaDoArquivo(campo);
+		cadastro2.cadastrarInstancias();
+		
+		ArrayList<Despesa> listaDespesa = despesaDAO.getLista();
+		System.out.println(listaDespesa.get(0).getTipoMovimentacao());
+		assertEquals(listaDespesa.get(0).getTipoMovimentacao(), "TipoMov");
+		
+	}
+	
+	@Test
+	public void deveRetornarUmaDespesaCadastradoPara2010() throws ParseException, SQLException {
+		String campo[] = new String[50];
+		campo[3] = "123";
+		campo[4] = "Cargo";
+		campo[14] = "TipoMov";
+		campo[8] = "TipoDoc";
+		campo[16] = "FormaPag";
+		campo[9] = "1234";
+		campo[12] = "Data";
+		campo[10] = "12345";
+		campo[11] = "Nome";
+		campo[13] = "123456";
+		campo[17] = "Descricao";
+		
+		/* //Indices não utilizados em 2010
+		Integer ano;
+		campo[25] = "ano";
+		campo[26] = "2010";
+		*/
+		
+		cadastro3.executarLinhaDoArquivo(campo);
+		cadastro3.cadastrarInstancias();
+		
+		ArrayList<Despesa> listaDespesa = despesaDAO.getLista();
+		System.out.println(listaDespesa.get(0).getTipoMovimentacao());
+		assertEquals(listaDespesa.get(0).getTipoMovimentacao(), "TipoMov");
+	}
+
+}
